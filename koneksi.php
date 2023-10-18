4da1b17ff66e5af0247a4227a9b39b9d8cbd762b
talitha30 <56035592+talitha30@users.noreply.github.com>
2023-10-18 07:40:03 +0700

    Delete koneksi.php

--- koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$databaseHost = 'localhost';
-$databaseName = 'kegiatan';
-$databaseUsername = 'root';
-$databasePassword = '';
-
-$mysqli = mysqli_connect($databaseHost, $databaseUsername, 
-    $databasePassword, $databaseName);
-?>
